b66f7cc839981fbe765cd552036eed78cd397b1c
AntonVoronezh <pstut@mail.ru>
2018-12-14 15:56:29 +0300

    add

--- src/js/task.js
+++ src/js/task.js
@@ -1,10 +1,9 @@
-const arr = ['99999999991d99', '555', '799', '8'];
+var arr = ['9kk9d97ssddddff', '55ssd5', '799', '8'];
 // fffffffffffff
 const rand = (min, max) => min + Math.floor(Math.random() * (max + 1 - min));
 // yyyuuuuuuuuuuuu
 const getRandElemFromArr = arr[rand(0, 3)];
-// yyyyyyyyyy
+// yyyyyyyyyyadffggd
 console.log(getRandElemFromArr);
 
-//
 
